5cc6f788e4c76d3fb5e1529853c870ce43daf0f7
3wnbr1 <ewen.brun@icloud.com>
2019-11-16 15:24:35 +0100

    Drive Node

--- src/modules/drive/include/drive/drive.hpp
+++ src/modules/drive/include/drive/drive.hpp
@@ -1,14 +1,58 @@
-#include "rclcpp/rclcpp.hpp"
+#ifndef DRIVE_NODE_HPP
+#define DRIVE_NODE_HPP
+
+
+#include <serial/serial.h>
+#include <rclcpp/rclcpp.hpp>
+#include "nav_msgs/msg/odometry.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "tf2_msgs/msg/tf_message.hpp"
 
 
 using namespace rclcpp;
 
 
-class Drive (rclcpp::Node()) {
+class Drive : public rclcpp::Node {
 public:
   Drive();
-  ~Drive();
+  // ~Drive();
 
 private:
+  struct tinySpeed {
+    uint8_t left = 0;
+    uint8_t right = 0;
+  };
+
+  // For communicating with ATTiny85 over UART
+  serial::Serial serial_interface_;
+  std::string serial_port_;
+  uint32_t serial_baudrate_;
+
+  // ROS time
+  rclcpp::Time time_since_last_sync_;
+
+  // ROS topic publishers
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
+  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_states_pub_;
+  rclcpp::Publisher<tf2_msgs::msg::TFMessage>::SharedPtr tf_pub_;
+
+  // ROS topic subscribers
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
+
+  rclcpp::TimerBase::SharedPtr timer_base_;
+
+  nav_msgs::msg::Odometry odom_;
+  sensor_msgs::msg::JointState joint_states_;
+
+  double wheel_separation_;
+  double wheel_radius_;
+
+  tinySpeed attiny_speed_cmd;
+
+  void init_parameters();
+  void init_variables();
+  void command_velocity_callback(const geometry_msgs::msg::Twist::SharedPtr cmd_vel_msg);
 
 };
+
+#endif /* DRIVE_NODE_HPP */
--- src/modules/drive/src/drive.cpp
+++ src/modules/drive/src/drive.cpp
@@ -1,14 +1,55 @@
-#include <unistd.h>
-#include <serial/serial.h>
-#include <rclcpp/rclcpp.hpp>
+#include <drive/drive.hpp>
 
 
-int main(int argc, char *argv[]) {
-  rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("drive_node");
+Drive::Drive() : Node("drive_node") {
+  /* Init parametrers from YAML */
+  init_parameters();
 
-  RCLCPP_INFO(node->get_logger(), "Hello drive !");
+  /* Give variables their initial values */
+  init_variables();
 
-  rclcpp::shutdown();
-  return 0;
+  /* Open UART connection */
+  serial::Serial serial_interface(this->serial_port_, this->serial_baudrate_);
+
+  /* Init ROS Publishers and Subscribers */
+  auto qos = rclcpp::QoS(rclcpp::KeepLast(10));
+
+  odom_pub_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", qos);
+  joint_states_pub_ = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", qos);
+  tf_pub_ = this->create_publisher<tf2_msgs::msg::TFMessage>("tf", qos);
+
+  cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>("cmd_vel", qos, std::bind(&Drive::command_velocity_callback, this, std::placeholders::_1));
+
+  RCLCPP_INFO(this->get_logger(), "Drive node initialised");
+}
+
+
+void Drive::init_parameters() {
+  // Declare parameters that may be set on this node
+  this->declare_parameter("serial_port");
+  this->declare_parameter("joint_states_frame");
+  this->declare_parameter("odom_frame");
+  this->declare_parameter("base_frame");
+  this->declare_parameter("wheels.separation");
+  this->declare_parameter("wheels.radius");
+
+  // Get parameters from yaml
+  this->get_parameter_or<std::string>("serial.port", serial_port_, "/dev/cu.usbserial-AI04Y24G");
+  this->get_parameter_or<uint32_t>("serial.baudrate", serial_baudrate_, 115200);
+
+  this->get_parameter_or<std::string>("joint_states_frame", joint_states_.header.frame_id, "base_footprint");
+  this->get_parameter_or<std::string>("odom_frame", odom_.header.frame_id, "odom");
+  this->get_parameter_or<std::string>("base_frame", odom_.child_frame_id, "base_footprint");
+  this->get_parameter_or<double>("wheels.separation", wheel_separation_, 0.0);
+  this->get_parameter_or<double>("wheels.radius", wheel_radius_, 0.0);
+}
+
+
+void Drive::init_variables() {
+
+}
+
+
+void Drive::command_velocity_callback(const geometry_msgs::msg::Twist::SharedPtr cmd_vel_msg) {
+
 }
