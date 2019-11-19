9c156a467a6df479a347389f3e0a6602b51328a5
Ewen BRUN <ewen.brun@icloud.com>
2019-11-19 17:29:35 +0100

    Teleop (#1)
    * urdf
    
    * urdf2
    
    * ur2f
    
    * Importation du teleop

--- src/asterix/launch/launch.py
+++ src/asterix/launch/launch.py
@@ -9,5 +9,8 @@ import launch_ros.actions
 
 
 def generate_launch_description():
-    main = launch_ros.actions.Node(package='asterix', node_executable='main', output='screen')
-    return launch.LaunchDescription([main])
+    urdf = os.path.join(get_package_share_directory('asterix','robot', 'asterix.urdf')
+
+    return launch.LaunchDescription([
+        Node(package='robot_state_publisher', node_executable='robot_state_publisher',output='screen', arguments=[urdf]),
+    ])
