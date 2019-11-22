a231296868eb32f91d499113ebc5a7f99f357bf7
3wnbr1 <ewen.brun@icloud.com>
2019-11-22 18:32:17 +0100

    Fix launchfile

--- src/asterix/launch/launch.py
+++ src/asterix/launch/launch.py
@@ -6,7 +6,8 @@
 
 import os
 import launch
-from launch_ros.actions import Node, get_package_share_directory
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
