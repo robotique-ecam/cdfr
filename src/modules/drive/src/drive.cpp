918575227396974129078c6f899e24df214bd53c
3wnbr1 <ewen.brun@icloud.com>
2019-11-23 21:31:11 +0100

    Fix : Negative speeds

--- src/modules/drive/src/drive.cpp
+++ src/modules/drive/src/drive.cpp
@@ -61,12 +61,12 @@ void Drive::init_variables() {
 
 
 void Drive::command_velocity_callback(const geometry_msgs::msg::Twist::SharedPtr cmd_vel_msg) {
-  uint8_t differential_speed_left = cmd_vel_msg->linear.x - (cmd_vel_msg->angular.z * wheel_separation_) / 2;
-  uint8_t differential_speed_right = cmd_vel_msg->linear.x + (cmd_vel_msg->angular.z * wheel_separation_) / 2;
+  int16_t differential_speed_left = cmd_vel_msg->linear.x - (cmd_vel_msg->angular.z * wheel_separation_) / 2;
+  int16_t differential_speed_right = cmd_vel_msg->linear.x + (cmd_vel_msg->angular.z * wheel_separation_) / 2;
 
   /* Set first bit of the ID according to differential_speed_cmd_ sign */
-  differential_speed_cmd_.left[2] = differential_speed_left;
-  differential_speed_cmd_.right[2] = differential_speed_right;
+  differential_speed_cmd_.left[2] = abs(differential_speed_left);
+  differential_speed_cmd_.right[2] = abs(differential_speed_right);
   differential_speed_cmd_.left[1] ^= (-signbit(differential_speed_left) ^ differential_speed_cmd_.left[1]) & 1;
   differential_speed_cmd_.right[1] ^= (-signbit(differential_speed_cmd_.right[1]) ^ differential_speed_cmd_.right[1]) & 1;
 
