0b085a8a333fe1725f9b4ce14db6ee87bc0ee3e0
3wnbr1 <ewen.brun@icloud.com>
2019-11-27 18:27:20 +0100

    Few fixes

--- src/modules/drive/include/drive/i2c.hpp
+++ src/modules/drive/include/drive/i2c.hpp
@@ -8,23 +8,22 @@ class I2C {
 public:
   int open(int i2c_bus) {
     sprintf(filename_, "/dev/i2c-%d", i2c_bus);
-    if ((file_i2c_ = open(filename_, O_RDWR)) < 0) {
+    if ((file_i2c_ = open(filename_, I2C_RDWR)) < 0) {
       /* Failed to open the i2c bus */
       return -1;
     }
   }
 
   int read_bytes(int addr, unsigned char buffer, int length) {
-    char *received_bytes;
     /* Failed to acquire bus access and/or read from slave */
-    if (set_address_(addr) && read(file_i2c_, buffer, length) != length) {
+    if (set_address_(addr) && fread(file_i2c_, buffer, length) != length) {
       return -1;
     }
   }
 
   int write_bytes(int addr, unsigned char buffer, int length) {
     /* Failed to acquire bus access and/or write from slave */
-    if (set_address_(addr) && write(file_i2c_, buffer, length) != length) {
+    if (set_address_(addr) && fwrite(file_i2c_, buffer, length) != length) {
       return -1;
     }
   }
@@ -34,7 +33,7 @@ private:
   char *filename_;
 
   int set_address_(int addr) {
-    if (ioctl(file_i2c, I2C_SLAVE, addr) < 0) {
+    if (ioctl(file_i2c_, I2C_SLAVE, addr) < 0) {
       return -1;
     }
   }
