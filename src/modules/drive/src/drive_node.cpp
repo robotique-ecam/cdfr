a64ee0e01942e3f9b9c4c8f298bad78d1edb8944
3wnbr1 <ewen.brun@icloud.com>
2019-11-30 14:03:58 +0100

    Removed debug

--- src/modules/drive/src/drive_node.cpp
+++ src/modules/drive/src/drive_node.cpp
@@ -5,8 +5,6 @@ int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   auto drive_node = std::make_shared<Drive>();
 
-  RCLCPP_INFO(drive_node->get_logger(), "Hello drive !");
-
   rclcpp::spin(drive_node);
   rclcpp::shutdown();
   return 0;
