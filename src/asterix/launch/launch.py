6c6b2f8d78a5bc1bd71bcfcd06f34be370c34a41
3wnbr1 <ewen.brun@icloud.com>
2019-12-17 15:28:43 +0100

    Sensors basecode

--- src/asterix/launch/launch.py
+++ src/asterix/launch/launch.py
@@ -12,9 +12,9 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     urdf = os.path.join(get_package_share_directory('asterix'), 'asterix.urdf')
-    asterix_drive_params = os.path.join(get_package_share_directory('drive'), 'param', 'asterix.yml')
+    asterix_params = os.path.join(get_package_share_directory('asterix'), 'param', 'asterix.yml')
 
     return launch.LaunchDescription([
-        Node(package='drive', node_executable='drive', output='screen', parameters=[asterix_drive_params]),
+        Node(package='drive', node_executable='drive', output='screen', parameters=[asterix_params]),
         Node(package='robot_state_publisher', node_executable='robot_state_publisher', output='screen', arguments=[urdf]),
     ])
