f95d71f9c35457ccf26296c26bbb9386cc48eca5
3wnbr1 <ewen.brun@icloud.com>
2020-01-08 19:04:10 +0100

    Merge master

---
+++ src/assurancetourix/strategix/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+"""I2C Interface ROS::2 package build tool."""
+
+
+from os import path
+from glob import glob
+from setuptools import setup, find_packages
+
+
+package_name = 'strategix'
+
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(),
+    data_files=[
+        (path.join('share', package_name), ['package.xml']),
+    ],
+    zip_safe=True,
+    install_requires=['setuptools'],
+    author='Ewen BRUN',
+    author_email='ewen.brun@ecam.fr',
+    maintainer='Ewen BRUN',
+    maintainer_email='ewen.brun@ecam.fr',
+    keywords=['ROS2', '', 'CDFR'],
+    description='Code node strategix.',
+    license='Funtech Makers :: CDFR 2020',
+    entry_points={
+        'console_scripts': [
+            'strategix = strategix.strategix:main',
+        ],
+    },
+)
