03fc332c4b451a54954a53aea908632229755c51
3wnbr1 <ewen.brun@icloud.com>
2020-01-27 21:41:57 +0100

    Just trying

--- src/assurancetourix/assurancetourix/src/assurancetourix.cpp
+++ src/assurancetourix/assurancetourix/src/assurancetourix.cpp
@@ -40,7 +40,7 @@ void Assurancetourix::get_image() {
     cv::Mat *image = new cv::Mat(cv::Size(width,(int)(height * 1.5)), CV_8UC1, buffer->data);
     cv::cvtColor(*image, *image, cv::COLOR_YUV2BGR_I420);
     arducam::arducam_release_buffer(buffer);
-    _frame = image;
+    _frame = *image;
 }
 #endif // MIPI_CAMERA
 
