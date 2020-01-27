69730cb71bc27508d62da01a6ae4bcdc1b975a5c
PhileasL <phileas.lambert@gmail.com>
2020-01-27 19:13:12 +0100

    ArUco color for rviz

--- src/assurancetourix/assurancetourix/src/assurancetourix.cpp
+++ src/assurancetourix/assurancetourix/src/assurancetourix.cpp
@@ -47,15 +47,26 @@ void Assurancetourix::init_parameters() {
   cv_img_bridge.encoding = "bgr8";
   cv_img_bridge.header.frame_id = "map";
 
+  //colors of the ArUco
+  blue_color_ArUco.r = 0.0;
+  blue_color_ArUco.g = 0.0;
+  blue_color_ArUco.b = 255.0;
+  blue_color_ArUco.a = 1.0;
+
+  yellow_color_ArUco.r = 255.0;
+  yellow_color_ArUco.g = 255.0;
+  yellow_color_ArUco.b = 0.0;
+  yellow_color_ArUco.a = 1.0;
+
   marker.header.frame_id = "map";
   marker.header.stamp.sec = 10;
   marker.header.stamp.nanosec = 100000000;
   marker.ns = "rviz";
-  marker.type = visualization_msgs::msg::Marker::SPHERE;
+  marker.type = visualization_msgs::msg::Marker::CUBE;
   marker.action = visualization_msgs::msg::Marker::ADD;
-  marker.scale.x = 0.1;
-  marker.scale.y = 0.1;
-  marker.scale.z = 0.1;
+  marker.scale.x = 0.07;
+  marker.scale.y = 0.07;
+  marker.scale.z = 0.01;
   marker.color.a = 1.0;
   marker.color.r = 0.0;
   marker.color.g = 250.0;
@@ -77,11 +88,11 @@ void Assurancetourix::init_parameters() {
   useless_point_vector.push_back(useless_point);
   marker.points = useless_point_vector;
 
-  useless_color.a = 1.0;
-  useless_color.r = 250.0;
-  useless_color.g = 0.0;
-  useless_color.b = 0.0;
-  useless_color_vector.push_back(useless_color);
+  default_color_ArUco.a = 0.0;
+  default_color_ArUco.r = 255.0;
+  default_color_ArUco.g = 255.0;
+  default_color_ArUco.b = 255.0;
+  useless_color_vector.push_back(default_color_ArUco);
   marker.colors = useless_color_vector;
 
   marker.frame_locked = false;
@@ -128,9 +139,9 @@ void Assurancetourix::_anotate_image(Mat img) {
 
       cv::aruco::drawAxis(img, _cameraMatrix, _distCoeffs, _rvecs[i], _tvecs[i], 0.1);
 
-      marker.pose.position.x = _tvecs[i].operator[](0);
-      marker.pose.position.y = _tvecs[i].operator[](1);
-      marker.pose.position.z = _tvecs[i].operator[](2);
+      marker.pose.position.x = 3*_tvecs[i].operator[](0);
+      marker.pose.position.y = 2*_tvecs[i].operator[](1);
+      marker.pose.position.z = _tvecs[i].operator[](2) * (-1);
 
       tf2::Quaternion q;
       q.setRPY(_rvecs[i].operator[](0), _rvecs[i].operator[](1), _rvecs[i].operator[](2));
@@ -140,7 +151,16 @@ void Assurancetourix::_anotate_image(Mat img) {
       marker.pose.orientation.z = q.z();
       marker.pose.orientation.w = q.w();
 
+      if ( (_detected_ids[i] <= 5) && (1 <= _detected_ids[i]) ){
+        marker.color = blue_color_ArUco;
+      }
+      else if ( (_detected_ids[i] <= 10) && (6 <= _detected_ids[i]) ){
+        marker.color = yellow_color_ArUco;
+      }
+      else {marker.color = default_color_ArUco;}
       marker.id = _detected_ids[i];
+
+
 
       marker_pub_->publish(marker);
       RCLCPP_INFO(this->get_logger(), "id: %d", _detected_ids[i]);
