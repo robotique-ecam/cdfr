41cdae0991eaf4650c854ec57a9bb56117f98c0f
Ewen BRUN <ewen.brun@icloud.com>
2020-01-28 23:53:58 +0100

    Add Sensors (#10)
    * Sensors basecode
    
    * Sensor node 
    HCSR04-I2C Support
    URDF extension
    
    * Updated I2C Bus

--- src/modules/sensors/include/sensors.hpp
+++ src/modules/sensors/include/sensors.hpp
@@ -1,14 +1,17 @@
-#ifndef SENSORS_HEADER_HPP
+#ifndef SENSORS_HEADER_HPP    // PQ pas Node
 #define SENSORS_HEADER_HPP
 
-#include <string>
+#include <vector>
+#include <chrono>
 #include <rclcpp/rclcpp.hpp>
-
+#include <sensor_msgs/msg/range.hpp>
 #ifndef SIMULATION
-#include "i2c.hpp"
-#endif /* SIMULAtION */
-#include "sensor_msgs/msg/range.hpp"
+  #include "i2c.hpp"
+#endif
 
+
+using namespace rclcpp;
+using namespace std::chrono;
 
 
 class Sensors : public rclcpp::Node {
@@ -17,17 +20,28 @@ public:
   ~Sensors();
 
 private:
+
+
+  int i2c_bus;
+
   #ifndef SIMULATION
   // For communicating with sensors over I2C
-  int i2c_bus;
   std::shared_ptr<I2C> i2c;
   #endif /* SIMULAtION */
 
+  std::vector <uint8_t> sensor_addresses;
+  std::vector <std::string> sensor_frames;
+
   // ROS topic publishers
+  rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<sensor_msgs::msg::Range>::SharedPtr sensors_pub_;
+  sensor_msgs::msg::Range hcsr_range_msg;
+
 
   void init_variables();
   void init_parameters();
+  void receive_distance();
+
 };
 
 #endif /* SENSORS_HEADER_HPP */
