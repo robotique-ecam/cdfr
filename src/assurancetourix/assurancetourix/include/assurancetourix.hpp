bf649378928afd4eb30222ede5406681c322a2bc
PhileasL <phileas.lambert@gmail.com>
2020-01-29 15:53:48 +0100

    Arrows for robots, cubes for game elements

--- src/assurancetourix/assurancetourix/include/assurancetourix.hpp
+++ src/assurancetourix/assurancetourix/include/assurancetourix.hpp
@@ -10,7 +10,6 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/aruco.hpp>
-#include <opencv2/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 #include <visualization_msgs/msg/marker.hpp>
@@ -18,7 +17,6 @@
 #include <geometry_msgs/msg/point.hpp>
 #include <std_msgs/msg/color_rgba.hpp>
 #include "arducam_mipicamera.hpp"
-
 
 using namespace rclcpp;
 using namespace cv;
@@ -34,6 +32,7 @@ private:
   void init_parameters();
   void _detect_aruco(Mat img);
   void _anotate_image(Mat img);
+  void set_vision_for_rviz(double color[3], double scale[3], uint type);
 
   #ifdef MIPI_CAMERA
   arducam::CAMERA_INSTANCE camera_instance;
@@ -51,7 +50,6 @@ private:
 
   std::vector<cv::Vec3d> _rvecs, _tvecs;
 
-  /* TODO: establish the new coeffss with the camera */
   double mat_dist_coeffs[1][5] = {{0.3764196381675952, 0.04110048800291252, 0.22561542466361806, -0.04945838920708183, -0.4677971323451955}};
   double mat_camera_matrix_coeff[3][3] = {{3667.6295530241346, 0.0, 1309.653212735876}, {0.0, 3009.8721997418875, 1565.0383212417412}, {0.0, 0.0, 1.0}};
 
@@ -70,15 +68,15 @@ private:
   rclcpp::QoS qos = rclcpp::QoS(rclcpp::KeepLast(10));
   rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
 
-  //useless point and colors, just to fill up the vector of points for the pointer
-  geometry_msgs::msg::Point useless_point;
-  std::vector<geometry_msgs::msg::Point> useless_point_vector;
+  double blue_color_ArUco[3] = {0, 0, 255};
+  double yellow_color_ArUco[3] = {255, 255, 0};
+  double default_color_ArUco[3] = {120, 120, 120};
+  double arrow_scale[3] = {0.1,0.1,0.1};
+  double game_elements_scale[3] = {0.7,0.7,0.01};
+  uint robot_type = 0;
+  uint game_element_type = 1;
 
-  std::vector<std_msgs::msg::ColorRGBA> useless_color_vector;
 
-  std_msgs::msg::ColorRGBA blue_color_ArUco;
-  std_msgs::msg::ColorRGBA yellow_color_ArUco;
-  std_msgs::msg::ColorRGBA default_color_ArUco;
   // Parameters
   int _camera_id;
 };
--- src/assurancetourix/assurancetourix/src/assurancetourix.cpp
+++ src/assurancetourix/assurancetourix/src/assurancetourix.cpp
@@ -10,7 +10,6 @@ Assurancetourix::Assurancetourix() : Node("assurancetourix") {
   arducam::arducam_set_mode(camera_instance, 2); // width: 1920, height: 1080, pixelformat: pBAA, desc: (null)
   arducam::arducam_software_auto_white_balance(camera_instance, 1);
   arducam::arducam_software_auto_exposure(camera_instance, 1);
-  // rclcpp::sleep_for(1s);
   #else // Standard camera
   _cap.open(_api_id + _camera_id);
   if (!_cap.isOpened()) {
@@ -48,60 +47,16 @@ void Assurancetourix::get_image() {
 
 
 void Assurancetourix::init_parameters() {
+  // initialisation of image message
   cv_img_bridge.encoding = "bgr8";
   cv_img_bridge.header.frame_id = "map";
 
-  //colors of the ArUco
-  blue_color_ArUco.r = 0.0;
-  blue_color_ArUco.g = 0.0;
-  blue_color_ArUco.b = 255.0;
-  blue_color_ArUco.a = 1.0;
-
-  yellow_color_ArUco.r = 255.0;
-  yellow_color_ArUco.g = 255.0;
-  yellow_color_ArUco.b = 0.0;
-  yellow_color_ArUco.a = 1.0;
-
+  // initialisation of marker message
   marker.header.frame_id = "map";
-  marker.header.stamp = this->get_clock()->now();
-  marker.ns = "rviz";
-  marker.type = visualization_msgs::msg::Marker::CUBE;
-  marker.action = visualization_msgs::msg::Marker::ADD;
-  marker.scale.x = 0.07;
-  marker.scale.y = 0.07;
-  marker.scale.z = 0.01;
   marker.color.a = 1.0;
-  marker.color.r = 0.0;
-  marker.color.g = 250.0;
-  marker.color.b = 0.0;
+  marker.action = visualization_msgs::msg::Marker::ADD;
   marker.lifetime.sec = 0;
   marker.lifetime.nanosec = 500000000;
-  marker.pose.position.x = 0;
-  marker.pose.position.y = 0;
-  marker.pose.position.z = 0;
-  marker.pose.orientation.x = 0;
-  marker.pose.orientation.y = 0;
-  marker.pose.orientation.z = 0;
-  marker.pose.orientation.w = 0;
-  marker.id = 0;
-
-  useless_point.x = 0;
-  useless_point.y = 1;
-  useless_point.z = 0;
-  useless_point_vector.push_back(useless_point);
-  marker.points = useless_point_vector;
-
-  default_color_ArUco.a = 1.0;
-  default_color_ArUco.r = 255.0;
-  default_color_ArUco.g = 255.0;
-  default_color_ArUco.b = 255.0;
-  useless_color_vector.push_back(default_color_ArUco);
-  marker.colors = useless_color_vector;
-
-  marker.frame_locked = false;
-  marker.text = "useless";
-  marker.mesh_resource = "null";
-  marker.mesh_use_embedded_materials = false;
 }
 
 
@@ -111,18 +66,17 @@ void Assurancetourix::detect() {
   #else
   _cap.read(_frame);
   #endif // MIPI_CAMERA
+
   _frame.copyTo(_anotated);
+
   cv_img_bridge.header.stamp = this->get_clock()->now();
   marker.header.stamp = this->get_clock()->now();
-
 
   _detect_aruco(_anotated);
   _anotate_image(_anotated);
 
-
   cv_img_bridge.image = _anotated;
   cv_img_bridge.toImageMsg(img_msg);
-
 
   image_pub_.publish(img_msg);
 }
@@ -132,6 +86,15 @@ void Assurancetourix::_detect_aruco(Mat img) {
   cv::aruco::detectMarkers(img, _dictionary, _marker_corners, _detected_ids, _parameters, _rejected_candidates);
 }
 
+void Assurancetourix::set_vision_for_rviz(double color[3], double scale[3], uint type) {
+  marker.type = type;
+  marker.color.r = color[0];
+  marker.color.g = color[1];
+  marker.color.b = color[2];
+  marker.scale.x = scale[0];
+  marker.scale.y = scale[1];
+  marker.scale.z = scale[2];
+}
 
 void Assurancetourix::_anotate_image(Mat img) {
   if (_detected_ids.size() > 0) {
@@ -143,9 +106,9 @@ void Assurancetourix::_anotate_image(Mat img) {
 
       cv::aruco::drawAxis(img, _cameraMatrix, _distCoeffs, _rvecs[i], _tvecs[i], 0.1);
 
-      marker.pose.position.x = 3*_tvecs[i].operator[](0);
-      marker.pose.position.y = 3*_tvecs[i].operator[](1);
-      marker.pose.position.z = 3*_tvecs[i].operator[](2);
+      marker.pose.position.x = _tvecs[i].operator[](0);
+      marker.pose.position.y = _tvecs[i].operator[](1);
+      marker.pose.position.z = _tvecs[i].operator[](2);
 
       double angle = norm(_rvecs[i]);
       Vec3d axis = _rvecs[i] / angle;
@@ -159,12 +122,14 @@ void Assurancetourix::_anotate_image(Mat img) {
       marker.pose.orientation.w = q.w();
 
       if ( (_detected_ids[i] <= 5) && (1 <= _detected_ids[i]) ){
-        marker.color = blue_color_ArUco;
+        set_vision_for_rviz(blue_color_ArUco, arrow_scale, robot_type);
       }
       else if ( (_detected_ids[i] <= 10) && (6 <= _detected_ids[i]) ){
-        marker.color = yellow_color_ArUco;
+        set_vision_for_rviz(yellow_color_ArUco, arrow_scale, robot_type);
       }
-      else {marker.color = default_color_ArUco;}
+      else {set_vision_for_rviz(default_color_ArUco, game_elements_scale, game_element_type);
+      }
+
       marker.id = _detected_ids[i];
 
       marker_pub_->publish(marker);
