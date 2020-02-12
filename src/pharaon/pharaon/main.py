646b22268f8ddae497b54910606d58b80b44678b
Ewen BRUN <ewen.brun@icloud.com>
2020-02-12 13:55:33 +0100

    Add Pharaon (#11)
    * Pharaon service
    
    * modif request

--- src/pharaon/pharaon/main.py
+++ src/pharaon/pharaon/main.py
@@ -1,8 +1,53 @@
 #!/usr/bin python3
 
 
-"""Main Node containing action server for Pharaon."""
+"""Service Node for Pharaon."""
 
 
-def main():
-    pass
+from rclpy.node import Node
+import rclpy
+import bluetooth
+
+
+bd_addr = "00:14:03:06:61:BA"
+port = 1
+
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+sock.connect((bd_addr, port))
+
+
+class PharaonService(Node):
+
+    def __init__(self):
+        super().__init__('pharaon_service')
+        bd_addr = "00:14:03:06:61:BA"
+        port = 1
+        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        sock.connect(bd_addr, port)
+        sock.bind(("", port))
+        sock.listen(1)
+
+    def activate_callback(self, request, response):
+        self.get_logger().info(str(request))
+        if False:
+            sock.send("demandeStatus;")
+            client_sock, address = sock.accept()
+            print("Accepted connection from ", address)
+            data = client_sock.recv(1024)
+            return ("reçu: ", data)
+
+        if (data == "deploy"):
+            sock.send("deploy;")
+            return ("deployed")
+
+    def __del__(self):
+        sock.close()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    pharaon_service = PharaonService()  # on instancie
+
+    rclpy.spin(pharaon_service)
+    rclpy.shutdown()
