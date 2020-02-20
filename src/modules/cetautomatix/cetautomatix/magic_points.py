28bc49a256e91efb6bf38e0b32880e4ccb1efaf8
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2020-02-20 13:53:40 +0100

    Separated points from the plotting code

--- src/modules/cetautomatix/cetautomatix/magic_points.py
+++ src/modules/cetautomatix/cetautomatix/magic_points.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import matplotlib.pyplot as plt
-
 """
 High level representation of Points of Interest on CDR2020 Table (to be used in Behavior Trees).
 Points in (x, y, a) for x & y in meters, a in degrees.
@@ -9,7 +7,6 @@ Zones in (x0, y0, x1, y1) for x & y in meters.
 """
 
 elements = {
-    # Points:
     "ECUEIL_BLEU": (0, 0.4, 180),
     "ECUEIL_JAUNE": (3, 0.4, 0),
     "ECUEIL_1": (0.85, 2, 90),
@@ -61,58 +58,3 @@ elements = {
               "PORT_JAUNE_1": (2.6, 0.93, 3, 1.47),
               "PORT_JAUNE_2": (1.15, 0, 1.25, 0.3)}
 }
-
-plt.axes()
-border = plt.Rectangle((0, 0), 3, 2, ec='black', fc='w', lw=5)
-plt.gca().add_patch(border)
-for key, value in elements.items():
-    if "ECUEIL" in key:
-        if "BLEU" in key:
-            line = plt.Line2D((value[0], value[0]), (value[1] - 0.2095, value[1] + 0.2095), lw=5, color='b')
-            plt.gca().add_line(line)
-        elif "JAUNE" in key:
-            line = plt.Line2D((value[0], value[0]), (value[1] - 0.2095, value[1] + 0.2095), lw=5, color='y')
-            plt.gca().add_line(line)
-        else:
-            line = plt.Line2D((value[0] - 0.2095, value[0] + 0.2095), (value[1], value[1]), lw=5, color='grey')
-            plt.gca().add_line(line)
-    elif "ARUCO" in key:
-        aruco = plt.Rectangle((value[0] - 0.05, value[1] - 0.05), 0.1, 0.1, ec='black', fc='w', lw=2)
-        plt.gca().add_patch(aruco)
-    elif "MANCHE" in key:
-        line = plt.Line2D((value[0] - 0.05, value[0] + 0.05), (value[1], value[1]), lw=5, color='grey')
-        plt.gca().add_line(line)
-    elif "PHARE" in key:
-        if "BLEU" in key:
-            line = plt.Line2D((value[0] - 0.225, value[0] + 0.225), (value[1], value[1]), lw=5, color='b')
-            plt.gca().add_line(line)
-        else:
-            line = plt.Line2D((value[0] - 0.225, value[0] + 0.225), (value[1], value[1]), lw=5, color='y')
-            plt.gca().add_line(line)
-    elif "GOB" in key:
-        for key2, value2 in value.items():
-            if "VERT" in key:
-                circle = plt.Circle(value2, radius=0.03, fc='g')
-                plt.gca().add_patch(circle)
-            else:
-                circle = plt.Circle(value2, radius=0.03, fc='r')
-                plt.gca().add_patch(circle)
-    elif "ZONES" in key:
-        for key2, value2 in value.items():
-            if "CHENAL" in key2:
-                if "ROUGE" in key2:
-                    chenal = plt.Rectangle((value2[0], value2[1]), value2[2] - value2[0], value2[3] - value2[1], fc='r')
-                    plt.gca().add_patch(chenal)
-                else:
-                    chenal = plt.Rectangle((value2[0], value2[1]), value2[2] - value2[0], value2[3] - value2[1], fc='g')
-                    plt.gca().add_patch(chenal)
-            elif "PORT" in key2:
-                if "BLEU" in key2:
-                    port = plt.Rectangle((value2[0], value2[1]), value2[2] - value2[0], value2[3] - value2[1], ec='b', fc='w')
-                    plt.gca().add_patch(port)
-                else:
-                    port = plt.Rectangle((value2[0], value2[1]), value2[2] - value2[0], value2[3] - value2[1], ec='y', fc='w')
-                    plt.gca().add_patch(port)
-
-plt.axis('scaled')
-plt.show()
