2fb47844346fda03857bfe6a057fd2f6f51cb3b0
3wnbr1 <ewen.brun@icloud.com>
2020-02-25 22:26:56 +0100

    Fix duplicate code block

--- src/pharaon/pharaon/pharaon/main.py
+++ src/pharaon/pharaon/pharaon/main.py
@@ -14,26 +14,24 @@ from pharaon_msgs.action import Pharaon
 bd_addr = "00:14:03:06:61:BA"
 port = 1
 
-sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-sock.connect((bd_addr, port))
-
 
 class PharaonActionServer(Node):
 
     def __init__(self):
         super().__init__('pharaon_action_server')
-        bd_addr = "00:14:03:06:61:BA"
-        port = 1
-        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        sock.connect(bd_addr, port)
-        sock.bind(("", port))
-        sock.listen(1)
+        self.bd_addr = "00:14:03:06:61:BA"
+        self.port = 1
+        self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        self.sock.connect(self.bd_addr, self.port)
+        self.sock.bind(("", self.port))
+        self.sock.listen(1)
         self._action_server = ActionServer(self, Pharaon, 'pharaon', self.activate_callback)
+        self.get_logger().info('Pharaon Action Server Initialized')
 
     def activate_callback(self, goal_handle):
         self.get_logger().info('Commanding Pharaon to deploy...')
         # Sending deploy over BT socket
-        sock.send("deploy;")
+        self.sock.send("deploy;")
 
         feedback_msg = Pharaon.Feedback()
         feedback_msg.deploying = True
@@ -41,8 +39,8 @@ class PharaonActionServer(Node):
 
         data = ""
         while 'deployed' not in data:
-            sock.send("demandeStatus;")
-            data = sock.recv(1024)
+            self.sock.send("demandeStatus;")
+            data = self.sock.recv(1024)
 
         goal_handle.succeed()
 
@@ -51,7 +49,7 @@ class PharaonActionServer(Node):
         return result
 
     def __del__(self):
-        sock.close()
+        self.sock.close()
 
 
 def main(args=None):
