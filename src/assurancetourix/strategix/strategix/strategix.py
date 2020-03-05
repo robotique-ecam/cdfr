8ac8b26d88ff8441bf50e9f10f44de37d0e8a2f6
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2020-03-05 17:21:06 +0100

    Added server & client

--- src/assurancetourix/strategix/strategix/strategix.py
+++ src/assurancetourix/strategix/strategix/strategix.py
@@ -1,34 +1,42 @@
 #!/usr/bin/env python3
 
-from strategix.srv import StrategixSrv
+from strategix_msgs.action import StrategixAction
 import rclpy
 from rclpy.node import Node
+from rclpy.action import ActionServer
 from score import Score
 
 
-class Strategix(Node):
+class StrategixActionServer(Node):
     def __init__(self):
-        super().__init__('strategix')
+        super().__init__('strategix_action_server')
         self.score = Score()
-        self.srv = self.create_service(StrategixSrv, 'strategix', self.send_response)
+        self._action_server = ActionServer(self, StrategixAction, 'strategix', self.execute_callback)
 
-    def send_response(self, request, response):
-        response.todo = None
-        print("Received: " + request.request, request.object + " from " + request.robot)
-        if request.request == 'todo':
-            response.todo = self.score.todoList
-        elif request.request == 'preempt':
-            self.score.preempt(request.object)
-        elif request.request == 'release':
-            self.score.release(request.object)
-        elif request.request == 'finish':
-            self.score.finish(request.object)
-        return response
+    def execute_callback(self, goal_handle):
+        self.get_logger().info('Executing goal...')
+        todo = None
+        if goal_handle.request.request == 'todo':
+            todo = self.score.todoList
+        elif goal_handle.request.request == 'preempt':
+            self.score.preempt(goal_handle.request.object)
+        elif goal_handle.request.request == 'release':
+            self.score.release(goal_handle.request.object)
+        elif goal_handle.request.request == 'finish':
+            self.score.finish(goal_handle.request.object)
+        goal_handle.succeed()
+        result = StrategixAction.Result()
+        result.todo = todo
+        return result
 
 
 def main(args=None):
     rclpy.init(args=args)
-    strategix = Strategix()
+    strategix = StrategixActionServer()
     rclpy.spin(strategix)
     strategix.destroy_node()
     rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
