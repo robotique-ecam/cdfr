7f73e0cb5e6772cce2ce9106c6cf91e13d206c52
FrozenHack <FrozenHack@fUnknown.local>
2020-03-05 17:20:17 +0100

    L2CAP, bluetooth.btcommon.BluetoothError: [Errno 111] Connection refused

---
+++ src/pharaon/pharaon/main.py
@@ -0,0 +1,54 @@
+#!/usr/bin python3
+
+
+"""Service Node for Pharaon."""
+
+
+from rclpy.node import Node
+import rclpy
+import bluetooth
+from std_srvs.srv import Trigger
+
+
+sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+
+
+class PharaonService(Node):
+
+    def __init__(self):
+        super().__init__('pharaon_service')
+        self.srv = self.create_service(
+            Trigger, 'activate', self.activate_callback)
+        bd_addr = "00:14:03:06:61:BA"
+        port = 1
+        sock.connect((bd_addr, port))
+        sock.bind((" ", port))
+        sock.listen(1)
+
+    def activate_callback(self, request, response):
+        self.get_logger().info(str(request))
+        if False:
+            sock.send("demandeStatus;")
+            client_sock, address = sock.accept()
+            print("Accepted connection from ", address)
+            data = client_sock.recv(1024)
+            return ("reçu: ", data)
+
+        if (data == "deploy"):
+            sock.send("deploy;")
+            return ("deployed")
+
+    def __del__(self):
+        sock.close()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    pharaon_service = PharaonService()  # on instancie
+
+    rclpy.spin(pharaon_service)
+    rclpy.shutdown()
+
+
+main()
