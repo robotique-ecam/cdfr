18ed172ecfa68a78222a81f8c57bd505fb2c29c4
3wnbr1 <ewen.brun@icloud.com>
2020-03-18 21:43:05 +0100

    Extended robot node

--- src/modules/cetautomatix/cetautomatix/robot.py
+++ src/modules/cetautomatix/cetautomatix/robot.py
@@ -6,17 +6,66 @@ from rclpy.node import Node
 from nav_msgs.msg import Odometry
 from cetautomatix.magic_points import elements
 from nav2_msgs.action._navigate_to_pose import NavigateToPose_Goal
+from strategix_msgs.srv import ChangeActionStatus, GetAvailableActions
 
 
 class Robot(Node):
     def __init__(self):
         super().__init__(node_name='robot')
+        robot = self.get_namespace()
         self.objective = "ARUCO42"
-        self.subscription = self.create_subscription(Odometry, '/odom', self.listener_callback, 10)
+        self._current_action = None
+        self._get_available_client = self.create_client(GetAvailableActions, '/strategix/available')
+        self._change_action_status_client = self.create_client(ChangeActionStatus, '/strategix/action')
+        self._get_available_request = GetAvailableActions.Request()
+        self._change_action_status_request = ChangeActionStatus.Request()
+        self._get_available_request.sender = robot
+        self._change_action_status_request.sender = robot
+        self._odom_sub = self.create_subscription(Odometry, '/odom', self._odom_callback, 1)
 
-    def listener_callback(self, msg):
+    def fetch_available_actions(self):
+        """Fetch available actions for BT."""
+        response = self._get_available_client.call(self._get_available_request)
+        return response.available
+
+    def preempt_action(self, action):
+        """Preempt an action for the BT."""
+        self._change_action_status_request.action = action
+        self._change_action_status_request.request = "PREEMPT"
+        response = self._change_action_status_request.call(self._change_action_status_request)
+        self._current_action = action if response.success else None
+        return response
+
+    def drop_current_action(self):
+        """Preempt an action for the BT."""
+        if self._current_action is None:
+            return False
+        self._change_action_status_request.action = self._current_action
+        self._change_action_status_request.request = "DROP"
+        response = self._change_action_status_request.call(self._change_action_status_request)
+        self._current_action = None
+        return response
+
+    def confirm_current_action(self):
+        """Preempt an action for the BT."""
+        if self._current_action is None:
+            return False
+        self._change_action_status_request.action = self._current_action
+        self._change_action_status_request.request = "CONFIRM"
+        response = self._change_action_status_request.call(self._change_action_status_request)
+        self._current_action = None
+        return response
+
+    def _odom_callback(self, msg):
         self.position = (msg.pose.pose.position.x, msg.pose.pose.position.y)
-        self.get_logger().info('I heard: "%s"' % msg.data)
+
+    def euler_to_quaternion(self, yaw, pitch, roll):
+        """Conversion between euler angles and quaternions."""
+        qx = np.sin(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) - np.cos(roll / 2) * np.sin(pitch / 2) * np.sin(yaw / 2)
+        qy = np.cos(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2) + np.sin(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2)
+        qz = np.cos(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2) - np.sin(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2)
+        qw = np.cos(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) + np.sin(roll / 2) * np.sin(pitch / 2) * np.sin(yaw / 2)
+        return [qx, qy, qz, qw]
 
     def compute_best_action(self, list):
         action_coeff_list = []
@@ -37,22 +86,10 @@ class Robot(Node):
                 best_action = action[0]
         self.objective = best_action
 
-    def euler_to_quaternion(self, yaw, pitch, roll):
-
-        qx = np.sin(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) - \
-            np.cos(roll / 2) * np.sin(pitch / 2) * np.sin(yaw / 2)
-        qy = np.cos(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2) + \
-            np.sin(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2)
-        qz = np.cos(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2) - \
-            np.sin(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2)
-        qw = np.cos(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) + \
-            np.sin(roll / 2) * np.sin(pitch / 2) * np.sin(yaw / 2)
-
-        return [qx, qy, qz, qw]
-
-    def getGoalPose(self):
+    def get_goal_pose(self, action):
+        """Get goal pose for action."""
         msg = NavigateToPose_Goal()
-        value = elements[self.objective]
+        value = elements[action]
         msg.pose.pose.position.z = 0.0
         msg.pose.pose.position.x = value[0]
         msg.pose.pose.position.y = value[1]
