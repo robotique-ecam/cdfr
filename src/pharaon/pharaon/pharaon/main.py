e7005e23285b4543e41ce6dec2bea70b41d43d0a
3wnbr1 <ewen.brun@icloud.com>
2020-03-19 16:12:06 +0100

    Prepare Pharaon for merge

--- src/pharaon/pharaon/pharaon/main.py
+++ src/pharaon/pharaon/pharaon/main.py
@@ -7,54 +7,48 @@
 import rclpy
 import bluetooth
 from rclpy.node import Node
-from rclpy.action import ActionServer
-from pharaon_msgs.action import Pharaon
+from std_srvs.srv import Trigger
 
 
-bd_addr = "00:14:03:06:61:BA"
+sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
 
 
-class PharaonActionServer(Node):
+class PharaonService(Node):
 
     def __init__(self):
-        super().__init__('pharaon_action_server')
-        self.bd_addr = bd_addr
-        self.port = 1
-        self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        self.sock.connect(self.bd_addr, self.port)
-        self.sock.bind(("", self.port))
-        self.sock.listen(1)
-        self._action_server = ActionServer(self, Pharaon, 'pharaon', self.activate_callback)
-        self.get_logger().info('Pharaon Action Server Initialized')
+        super().__init__('pharaon_service')
+        self.srv = self.create_service(
+            Trigger, 'activate', self.activate_callback)
+        bd_addr = "00:14:03:06:61:BA"
+        port = 0x1001
+        sock.connect((bd_addr, port))
+        sock.bind((" ", port))
+        sock.listen(1)
 
-    def activate_callback(self, goal_handle):
-        self.get_logger().info('Commanding Pharaon to deploy...')
-        # Sending deploy over BT socket
-        self.sock.send("deploy;")
+    def activate_callback(self, request, response):
+        self.get_logger().info(str(request))
+        if False:
+            sock.send("demandeStatus;")
+            client_sock, address = sock.accept()
+            print("Accepted connection from ", address)
+            data = client_sock.recv(1024)
+            return ("reçu: ", data)
 
-        feedback_msg = Pharaon.Feedback()
-        feedback_msg.deploying = True
-        goal_handle.publish_feedback(feedback_msg)
-
-        data = ""
-        while 'deployed' not in data:
-            self.sock.send("demandeStatus;")
-            data = self.sock.recv(1024)
-
-        goal_handle.succeed()
-
-        result = Pharaon.Result()
-        result.deployed = True
-        return result
+        if (data == "deploy"):
+            sock.send("deploy;")
+            return ("deployed")
 
     def __del__(self):
-        self.sock.close()
+        sock.close()
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    pharaon_service = PharaonActionServer()  # on instancie
+    pharaon_service = PharaonService()  # on instancie
 
     rclpy.spin(pharaon_service)
     rclpy.shutdown()
+
+
+main()
