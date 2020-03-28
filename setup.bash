c172f92ddfece52bead7f4aeac204d0f59dc0db1
3wnbr1 <ewen.brun@icloud.com>
2020-03-28 23:25:48 +0100

    Finalizing v0.7.0 ✅

--- setup.bash
+++ setup.bash
@@ -10,6 +10,7 @@ function print_success {
 
 function print_failure {
     (>&2 printf "\x1b[31m[!] ${1}\x1b[0m\n")
+    exit 1
 }
 
 function generate_urdfs {
@@ -34,15 +35,15 @@ fi
 
 if [ $robot = "asterix" ] || [ $robot = "obelix" ]; then
     print_info "Setting up $robot"
-    generate_urdfs && colcon build --symlink-install --cmake-args=" -DCMAKE_BUILD_TYPE=Release" --packages-skip assurancetourix strategix pharaon_msgs pharaon && print_success "Built packages for $robot" || print_failure "Packages build failed" && exit 1
+    generate_urdfs && colcon build --symlink-install --cmake-args=" -DCMAKE_BUILD_TYPE=Release" --packages-skip assurancetourix strategix pharaon_msgs pharaon && print_success "Built packages for $robot" || print_failure "Packages build failed"
 
 elif [ $robot = "assurancetourix" ]; then
     print_info "Setting up $robot"
-    colcon build --symlink-install --cmake-args " -DMIPI_CAMERA=ON" --cmake-args=" -DCMAKE_BUILD_TYPE=Release" --packages-select assurancetourix strategix_msgs strategix pharaon_msgs pharaon && print_success "Built packages for $robot" || print_failure "Packages build failed" && exit 1
+    colcon build --symlink-install --cmake-args " -DMIPI_CAMERA=ON" --cmake-args=" -DCMAKE_BUILD_TYPE=Release" --packages-select assurancetourix strategix_msgs strategix pharaon_msgs pharaon && print_success "Built packages for $robot" || print_failure "Packages build failed"
 
 elif [ $robot = "simulation" ]; then
     print_info "Setting up simulation environment"
-    generate_urdfs && colcon build --symlink-install --cmake-args=" -DCMAKE_BUILD_TYPE=Release" --cmake-args=" -DSIMULATION=ON" --packages-skip sensors && print_success "Built packages for $robot" || print_failure "Packages build failed" && exit 1
+    generate_urdfs && colcon build --symlink-install --cmake-args=" -DCMAKE_BUILD_TYPE=Release" --cmake-args=" -DSIMULATION=ON" --packages-skip sensors && print_success "Built packages for $robot" || print_failure "Packages build failed"
 
 else
     print_failure "No such component"
