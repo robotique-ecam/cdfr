74cc035f84e22d4bfccf8c584e29c9235a173afb
3wnbr1 <ewen.brun@icloud.com>
2020-03-28 23:06:36 +0100

    Finalizing launch files for v0.7.0 ✅

--- setup.bash
+++ setup.bash
@@ -6,64 +6,45 @@ function print_info {
 
 function print_success {
     (>&2 printf "\x1b[32m[+] ${1}\x1b[0m\n")
-    exit 0
 }
 
 function print_failure {
     (>&2 printf "\x1b[31m[!] ${1}\x1b[0m\n")
-    exit 1
 }
 
 function generate_urdfs {
-  xacro tools/xacro/asterix.xacro -o src/asterix/robot/asterix.urdf && xacro tools/xacro/obelix.xacro -o src/obelix/robot/obelix.urdf && print_success "Generated URDF files" || print_failure "Failled to generate URDFs"
+    xacro tools/xacro/asterix.xacro -o src/asterix/robot/asterix.urdf
+    xacro tools/xacro/obelix.xacro -o src/obelix/robot/obelix.urdf
+    if [[ $? -eq 0 ]]; then
+        print_success "Generated URDF files"
+        return 0
+    else
+        print_failure "Failled to generate URDFs"
+        return 1
+    fi
 }
 
-echo "[38;5;004m                                 /
-                               &
-                              %
-                 %%%%%%%%%%%%%%%%
-             .%%%%%%%%%%%%%%%%%%%%%%/
-            %%%%%%%    #%%%%%%%%%%%%%%(
-          ,%%%%%%%%#   %%%%%%,   %%%%%%%
-          #%%%%%%%%%%%%%%%%%%    %%%%%%%
-               .#%%%%%%%%%%%%%%%%%%%%%%%
-         (%%%#, &%%%%%%%%%%%%%%%%%%%%%%
- &%%%%%%%%%%%%%%%%%%%%%%%%%%#    (&%#
-    ##%%%%%%%%%%%    *%%%%%%%%%%%%#.
-       %%%%%%%%%%%.            (%%%%%
-      %%%%%%%%%%%%%*          *%%%%%%%/
-     %%%%%%%%%%%%%%%#        .%%%*(%%&
-    &%%%%%%%%%%%%%%%%%       %%%(
-   %%%%%%%%%%%%%%%%%%%%     %%%%
-  (%%%%%%%%%%%%%%%%%%%%%   %%%%
-     (%%%%%%%%%%%%%%%%%%% %%%%
-     %%%%%%%%%%%%%%%%%%%%%%%%
-     %%%%%%%%%%%%&         /
-        %%%%%%%.
+
+if [ -n "$1" ]; then
+    robot=$1
+else
+    read -p "Please enter the name of the robot to setup : " robot
+fi
 
 
-"
+if [ $robot = "asterix" ] || [ $robot = "obelix" ]; then
+    print_info "Setting up $robot"
+    generate_urdfs && colcon build --symlink-install --cmake-args=" -DCMAKE_BUILD_TYPE=Release" --packages-skip assurancetourix strategix pharaon_msgs pharaon && print_success "Built packages for $robot" || print_failure "Packages build failed"
 
-print_info "Welcome to Robot Setup Script"
-read -p "Please enter the name of the robot to setup : " robot
+elif [ $robot = "assurancetourix" ]; then
+    print_info "Setting up $robot"
+    colcon build --symlink-install --cmake-args " -DMIPI_CAMERA=ON" --cmake-args=" -DCMAKE_BUILD_TYPE=Release" --packages-select assurancetourix strategix_msgs strategix pharaon_msgs pharaon && print_success "Built packages for $robot" || print_failure "Packages build failed"
 
-if [ $robot = 'asterix' ] || [ $robot = 'obelix' ]; then
-
-  print_info "Setting up $robot"
-  generate_urdfs
-  colcon build --symlink-install --cmake-args=' -DCMAKE_BUILD_TYPE=Release' --packages-skip assurancetourix strategix pharaon_msgs pharaon && print_success "Built packages for $robot" || print_failure "Packages build failed"
-
-elif [ $robot = 'assurancetourix' ]; then
-
-  print_info "Setting up $robot"
-  colcon build --symlink-install --cmake-args ' -DMIPI_CAMERA=ON' --cmake-args=' -DCMAKE_BUILD_TYPE=Release' --packages-select assurancetourix strategix_msgs strategix pharaon_msgs pharaon && print_success "Built packages for $robot" || print_failure "Packages build failed"
-
-elif [ $robot = 'simulation' ]; then
-  generate_urdfs
-  colcon build --symlink-install --cmake-args=' -DCMAKE_BUILD_TYPE=Release' && print_success "Built packages for $robot" || print_failure "Packages build failed"
+elif [ $robot = "simulation" ]; then
+    print_info "Setting up simulation environment"
+    generate_urdfs && colcon build --symlink-install --cmake-args=" -DCMAKE_BUILD_TYPE=Release" --cmake-args=" -DSIMULATION=ON" && print_success "Built packages for $robot" || print_failure "Packages build failed"
 
 else
-
-  print_failure "No such component"
+    print_failure "No such component"
 
 fi
