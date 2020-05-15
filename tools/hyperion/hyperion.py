e0254a6125f6dd86cc060c3c299578daeefa4ffd
3wnbr1 <ewen.brun@icloud.com>
2020-05-15 11:45:12 +0200

    First hyperion commit

---
+++ tools/hyperion/hyperion.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+"""Hyperion system tests."""
+
+
+from score import Evaluator
+from utils import print_ok, print_warn, print_error, print_critical
---
+++ tools/hyperion/score.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+"""Contains aftermath Evaluator."""
+
+
+class Evaluator:
+    """Evaluator class."""
+
+    def __init__(self, side: str, cups_poses: dict, checks: dict):
+        """Create an evaluator."""
+        self.side = side
+        self.cups_poses = cups_poses
+        self.checks = checks
+
+    def get_score():
+        """Return the global score."""
+        return 0
