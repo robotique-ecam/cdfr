698b7b423ca3c27729b240ccf735f793182c7a25
3wnbr1 <ewen.brun@icloud.com>
2020-05-21 21:14:04 +0200

    Support to set side in strategix

---
+++ src/assurancetourix/strategix/strategix/exceptions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+"""Exceptions."""
+
+
+class MatchStartedException(Exception):
+    def __init__(self, message, errors):
+        super().__init__(message)
+        self.errors = 'Match has already been started'
--- src/assurancetourix/strategix/strategix/strategix.py
+++ src/assurancetourix/strategix/strategix/strategix.py
@@ -2,23 +2,41 @@
 
 
 import rclpy
+from rcl_interfaces.msg import SetParametersResult
 from rclpy.node import Node
+from strategix.exceptions import MatchStartedException
 from strategix.score import Score
 from strategix_msgs.srv import ChangeActionStatus, GetAvailableActions
-
-color = 'blue'
 
 
 class StrategixActionServer(Node):
     def __init__(self):
         super().__init__('strategix_action_server')
+        self.side = self.declare_parameter('side', 'blue')
+        self.add_on_set_parameters_callback(self._on_set_parameters)
         self.score = Score()
         self.todo_srv = self.create_service(GetAvailableActions, '/strategix/available', self.available_callback)
         self.action_srv = self.create_service(ChangeActionStatus, '/strategix/action', self.action_callback)
+        self.get_logger().info(f'Default side is {self.side.value}')
         self.get_logger().info('Strategix is ready')
 
     def excludeList(self):
-        return self.score.excludeFromBlue if color == 'blue' else self.score.excludeFromYellow
+        return self.score.excludeFromBlue if self.side.value == 'blue' else self.score.excludeFromYellow
+
+    def _on_set_parameters(self, params):
+        """Handle Parameter events especially for side."""
+        result = SetParametersResult()
+        try:
+            for param in params:
+                if param.name == 'side':
+                    self.get_logger().info(f'Side changed {param.value}')
+                    self.side = param
+                else:
+                    setattr(self, param.name, param)
+            result.successful = True
+        except MatchStartedException as e:
+            result.reason = e
+        return result
 
     def available_callback(self, request, response):
         self.get_logger().info('GET %s' % (request.sender))
