65013fd6a3f5bffc20fb2bba5942755003a0e1f9
3wnbr1 <ewen.brun@icloud.com>
2020-10-05 17:06:40 +0200

    Add I2C read byte support for pumps boards

--- firmware/Carte-Actionneurs/src/main.cpp
+++ firmware/Carte-Actionneurs/src/main.cpp
@@ -4,8 +4,14 @@
 #define ADDRESS 0x40
 
 void receiveEvent(int n) {
+  // Call upon I2C write byte from master device
   if (n == 1)
     PORTD = Wire.read();
+}
+
+void requestEvent() {
+  // Call upon I2C read byte from master device
+  Wire.write(PORTD);
 }
 
 void setup() {
@@ -13,6 +19,7 @@ void setup() {
   DDRD = 0xFF;
   Wire.begin(ADDRESS);
   Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
 }
 
 void loop() {}
