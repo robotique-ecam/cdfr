552da5000742fcef87f68161e2f65b030df721be
3wnbr1 <ewen.brun@icloud.com>
2020-10-07 23:55:01 +0200

    Fix actuators

--- src/modules/actuators/actuators/obelix.py
+++ src/modules/actuators/actuators/obelix.py
@@ -8,15 +8,15 @@ from actuators.actuators import DYNA_DOWN, DYNA_UP, NC, NO, Actuators
 
 # Pumps and valves adresses
 PUMPS = {
-    1: {'pump': 0, 'valve': 1, 'type': NO},
-    2: {'pump': 2, 'valve': 3, 'type': NO},
-    3: {'pump': 4, 'valve': 9, 'type': NO},
-    4: {'pump': 10, 'valve': 11, 'type': NO},
-    5: {'pump': 0, 'valve': 1, 'type': NC},
-    6: {'pump': 2, 'valve': 3, 'type': NC},
-    7: {'pump': 4, 'valve': 9, 'type': NC},
-    8: {'pump': 10, 'valve': 11, 'type': NC},
-    9: {'pump': 12, 'valve': 13, 'type': NC},
+    1: {'pump': 0, 'valve': 1, 'type': NC},
+    2: {'pump': 2, 'valve': 3, 'type': NC},
+    3: {'pump': 4, 'valve': 9, 'type': NC},
+    4: {'pump': 10, 'valve': 11, 'type': NC},
+    5: {'pump': 0, 'valve': 1, 'type': NO},
+    6: {'pump': 2, 'valve': 3, 'type': NO},
+    7: {'pump': 4, 'valve': 9, 'type': NO},
+    8: {'pump': 10, 'valve': 11, 'type': NO},
+    9: {'pump': 12, 'valve': 13, 'type': NO},
 }
 
 # Fans addresses
@@ -37,7 +37,7 @@ DYNAMIXELS = {
 
 actuators = Actuators(
     i2c_bus=5,
-    pump_addr=[0x41, 0x40],
+    pump_addr=[0x40, 0x41],
     FANS=FANS,
     PUMPS=PUMPS,
     SERVOS=SERVOS
