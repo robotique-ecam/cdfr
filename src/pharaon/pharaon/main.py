6cd67b3259d85190564d54328504da10e6c6dcad
3wnbr1 <ewen.brun@icloud.com>
2020-10-07 21:10:48 +0200

    Refactored pharaon

--- src/pharaon/pharaon/main.py
+++ src/pharaon/pharaon/main.py
@@ -4,13 +4,10 @@
 """Service Node for Pharaon."""
 
 
-import bluetooth
-
 import rclpy
+from bluedot.btcomm import BluetoothClient
 from rclpy.node import Node
 from std_srvs.srv import Trigger
-
-sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
 
 
 class PharaonService(Node):
@@ -18,30 +15,24 @@ class PharaonService(Node):
     def __init__(self):
         super().__init__('pharaon_service')
         self.srv = self.create_service(Trigger, 'activate', self.activate_callback)
-        bd_addr = '00:14:03:06:61:BA'
-        port = 0x1001
-        sock.connect((bd_addr, port))
-        sock.bind((' ', port))
-        sock.listen(1)
+        self.bt = BluetoothClient('00:14:03:06:61:BA', self.data_received)
+        self.get_logger().info('Pharaon has been started')
+
+    def data_received(self, data):
+        """Callback upon data received."""
+        self.get_logger().info(data)
 
     def activate_callback(self, request, response):
         """Callback called upon trigger."""
         try:
-            sock.send('deploy\n')
-            client_sock, address = sock.accept()
-            print('Accepted connection from ', address)
-            data = client_sock.recv(1024)
-            if 'deployed' in data:
-                response.success = True
-                response.message = 'Pharaon has been deployed'
+            self.bt.send('deploy;')
+            response.success = True
+            response.message = 'Pharaon requested to deploy'
             self.get_logger().info(response.message)
         except BaseException as e:
             response.success = False
             response.message = f'Pharaon failed to deploy with {e}'
             self.get_logger().warn(response.message)
-
-    def __del__(self):
-        sock.close()
 
 
 def main(args=None):
@@ -52,6 +43,3 @@ def main(args=None):
     except KeyboardInterrupt:
         pass
     rclpy.shutdown()
-
-
-main()
