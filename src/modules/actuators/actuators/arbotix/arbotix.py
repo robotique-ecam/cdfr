49c0fe47c4467db8e2e2a401f47ac68db195dfd7
3wnbr1 <ewen.brun@icloud.com>
2020-10-10 17:24:15 +0200

    Fixed dynamixels

--- src/modules/actuators/actuators/arbotix/arbotix.py
+++ src/modules/actuators/actuators/arbotix/arbotix.py
@@ -106,51 +106,42 @@ class ArbotiX:
         if d == b'':
             return None
 
+        # Need int, not byte
+        d = d[0]
+
         # now process our byte
         if mode == 0:           # get our first 0xFF
             if d == 0xff:
-                print('Oxff found')
                 return self.getPacket(1)
             else:
-                print(f'Oxff NOT found, restart: {d}')
                 return self.getPacket(0)
         elif mode == 1:         # get our second 0xFF
             if d == 0xff:
-                print('Oxff found')
                 return self.getPacket(2)
             else:
-                print(f'Oxff NOT found, restart: {d}')
                 return self.getPacket(0)
         elif mode == 2:         # get id
             if d != 0xff:
-                print(f'ID found: {d}')
                 return self.getPacket(3, d)
             else:
-                print('0xff is not ID, restart')
                 return self.getPacket(0)
         elif mode == 3:         # get length
-            print(f'Length found: {d}')
             return self.getPacket(4, id, d)
         elif mode == 4:         # read error
-            print(f'Error level found: {d}')
             self.error = d
             if leng == 2:
                 return self.getPacket(6, id, leng, d, [])
             else:
                 return self.getPacket(5, id, leng, d, [])
         elif mode == 5:         # read params
-            print(f'Parameter found: {d}')
             params.append(d)
             if len(params) + 2 == leng:
                 return self.getPacket(6, id, leng, error, params)
             else:
                 return self.getPacket(5, id, leng, error, params)
         elif mode == 6:         # read checksum
-            print(f'Checksum found: {d}')
             checksum = id + leng + error + sum(params) + d
-            print(f'Checksum computed: {checksum}')
             if checksum % 256 != 255:
-                print('Checksum ERROR')
                 return None
             return params
         # fail
