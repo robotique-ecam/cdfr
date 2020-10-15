9e50e355415c540bb1e5c398d465879412a25ecd
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2020-10-15 17:10:18 +0200

    Added support for specific robot actions

--- src/assurancetourix/strategix/strategix/actions.py
+++ src/assurancetourix/strategix/strategix/actions.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
 actions = {
-    "MANCHE1": {"ONLY_BLUE": True},
-    "MANCHE2": {"ONLY_BLUE": True},
-    "MANCHE3": {"ONLY_YELLOW": True},
-    "MANCHE4": {"ONLY_YELLOW": True},
+    "MANCHE1": {"ONLY_SIDE": "blue"},
+    "MANCHE2": {"ONLY_SIDE": "blue"},
+    "MANCHE3": {"ONLY_SIDE": "yellow"},
+    "MANCHE4": {"ONLY_SIDE": "yellow"},
     "PAVILLON": {"STATUS": "PREEMPTED"},
-    "ECUEIL_1": {"GOBS": ['GOB35', 'GOB36', 'GOB37', 'GOB38', 'GOB39']},
-    "ECUEIL_2": {"GOBS": ['GOB40', 'GOB41', 'GOB42', 'GOB43', 'GOB44']},
-    "ECUEIL_BLEU": {"ONLY_BLUE": True, "GOBS": ['GOB25', 'GOB26', 'GOB27', 'GOB28', 'GOB29']},
-    "ECUEIL_JAUNE": {"ONLY_YELLOW": True, "GOBS": ['GOB30', 'GOB31', 'GOB32', 'GOB33', 'GOB34']},
-    "PHARE_BLEU": {"ONLY_BLUE": True},
-    "PHARE_JAUNE": {"ONLY_YELLOW": True},
+    "ECUEIL_1": {"ONLY_ROBOT": "obelix", "GOBS": ['GOB35', 'GOB36', 'GOB37', 'GOB38', 'GOB39']},
+    "ECUEIL_2": {"ONLY_ROBOT": "obelix", "GOBS": ['GOB40', 'GOB41', 'GOB42', 'GOB43', 'GOB44']},
+    "ECUEIL_BLEU": {"ONLY_ROBOT": "obelix", "ONLY_SIDE": "blue", "GOBS": ['GOB25', 'GOB26', 'GOB27', 'GOB28', 'GOB29']},
+    "ECUEIL_JAUNE": {"ONLY_ROBOT": "obelix", "ONLY_SIDE": "yellow", "GOBS": ['GOB30', 'GOB31', 'GOB32', 'GOB33', 'GOB34']},
+    "PHARE_BLEU": {"ONLY_SIDE": "blue"},
+    "PHARE_JAUNE": {"ONLY_SIDE": "yellow"},
     # Red Cups
     'GOB2': {"COLOR": "RED"},
     'GOB3': {"COLOR": "RED"},
@@ -26,12 +26,12 @@ actions = {
     'GOB22': {"COLOR": "RED"},
     'GOB23': {"COLOR": "RED"},
     # Cups in ECUEIL_BLEU:
-    'GOB25': {"COLOR": "RED", "IN_ECUEIL": True, "ONLY_BLUE": True},
-    'GOB27': {"COLOR": "RED", "IN_ECUEIL": True, "ONLY_BLUE": True},
-    'GOB29': {"COLOR": "RED", "IN_ECUEIL": True, "ONLY_BLUE": True},
+    'GOB25': {"COLOR": "RED", "IN_ECUEIL": True, "ONLY_SIDE": "blue"},
+    'GOB27': {"COLOR": "RED", "IN_ECUEIL": True, "ONLY_SIDE": "blue"},
+    'GOB29': {"COLOR": "RED", "IN_ECUEIL": True, "ONLY_SIDE": "blue"},
     # Cups in ECUEIL_JAUNE:
-    'GOB31': {"COLOR": "RED", "IN_ECUEIL": True, "ONLY_YELLOW": True},
-    'GOB33': {"COLOR": "RED", "IN_ECUEIL": True, "ONLY_YELLOW": True},
+    'GOB31': {"COLOR": "RED", "IN_ECUEIL": True, "ONLY_SIDE": "yellow"},
+    'GOB33': {"COLOR": "RED", "IN_ECUEIL": True, "ONLY_SIDE": "yellow"},
     # Cups in ECUEIL_1 & ECUEIL_2 following the Scenario 1:
     'GOB36': {"COLOR": "RED", "IN_ECUEIL": True},
     'GOB39': {"COLOR": "RED", "IN_ECUEIL": True},
@@ -52,12 +52,12 @@ actions = {
     'GOB21': {"COLOR": "GREEN"},
     'GOB24': {"COLOR": "GREEN"},
     # Cups in ECUEIL_BLEU:
-    'GOB26': {"COLOR": "GREEN", "IN_ECUEIL": True, "ONLY_BLUE": True},
-    'GOB28': {"COLOR": "GREEN", "IN_ECUEIL": True, "ONLY_BLUE": True},
+    'GOB26': {"COLOR": "GREEN", "IN_ECUEIL": True, "ONLY_SIDE": "blue"},
+    'GOB28': {"COLOR": "GREEN", "IN_ECUEIL": True, "ONLY_SIDE": "blue"},
     # Cups in ECUEIL_JAUNE:
-    'GOB30': {"COLOR": "GREEN", "IN_ECUEIL": True, "ONLY_YELLOW": True},
-    'GOB32': {"COLOR": "GREEN", "IN_ECUEIL": True, "ONLY_YELLOW": True},
-    'GOB34': {"COLOR": "GREEN", "IN_ECUEIL": True, "ONLY_YELLOW": True},
+    'GOB30': {"COLOR": "GREEN", "IN_ECUEIL": True, "ONLY_SIDE": "yellow"},
+    'GOB32': {"COLOR": "GREEN", "IN_ECUEIL": True, "ONLY_SIDE": "yellow"},
+    'GOB34': {"COLOR": "GREEN", "IN_ECUEIL": True, "ONLY_SIDE": "yellow"},
     # Cups in ECUEIL_1 & ECUEIL_2 following the Scenario 1:
     'GOB35': {"COLOR": "GREEN", "IN_ECUEIL": True},
     'GOB37': {"COLOR": "GREEN", "IN_ECUEIL": True},
