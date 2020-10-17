96fc02cdb67088e4f567f7e887d2e4f0bc8ed7fb
3wnbr1 <ewen.brun@icloud.com>
2020-10-17 11:50:35 +0200

    Crude simulation mode for sensors

--- src/modules/sensors/include/sensors.hpp
+++ src/modules/sensors/include/sensors.hpp
@@ -22,15 +22,14 @@ private:
   int i2c_bus;
 
 #ifndef SIMULATION
-  // For communicating with sensors over I2C
   std::shared_ptr<I2C> i2c;
+  std::vector<LINUX_VL53L1X> vl53l1x_sensors;
 #endif /* SIMULAtION */
 
   std::vector<uint8_t> hcsr04_addresses;
   std::vector<std::string> hcsr04_frames;
   std::vector<uint8_t> vl53l1x_addresses;
   std::vector<std::string> vl53l1x_frames;
-  std::vector<LINUX_VL53L1X> vl53l1x_sensors;
 
   // ROS topic publishers
   rclcpp::TimerBase::SharedPtr timer_;
