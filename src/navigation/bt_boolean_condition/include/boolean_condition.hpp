f2cf30655f31185fcac7eb0f98c029b9f0d94817
PhileasL <phileas.lambert@gmail.com>
2021-03-07 20:32:59 +0100

    Custom bt plugin: bt_boolean_condition

---
+++ src/navigation/bt_boolean_condition/include/boolean_condition.hpp
@@ -0,0 +1,39 @@
+#ifndef BOOLEAN_CONDITION_ACTION_HPP_
+#define BOOLEAN_CONDITION_ACTION_HPP_
+
+#include <memory>
+#include <string>
+
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "nav_msgs/msg/path.hpp"
+
+#include "behaviortree_cpp_v3/action_node.h"
+
+namespace nav2_behavior_tree
+{
+
+class BooleanCondition : public BT::ActionNodeBase
+{
+public:
+  BooleanCondition(
+    const std::string & xml_tag_name,
+    const BT::NodeConfiguration & conf);
+
+
+  static BT::PortsList providedPorts()
+  {
+    return {
+      BT::InputPort<bool>("boolean", "SUCCESS if true, else FAILURE"),
+    };
+  }
+
+private:
+  void halt() override {}
+  BT::NodeStatus tick() override;
+
+  bool bool_;
+};
+
+}  // namespace nav2_behavior_tree
+
+#endif  // BOOLEAN_CONDITION_ACTION_HPP_
