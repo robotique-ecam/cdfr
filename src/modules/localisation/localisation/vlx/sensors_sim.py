7f24c966aeab0b92f2c75a0e7bba2ef8d9cf871b
PhileasL <phileas.lambert@gmail.com>
2021-05-01 06:44:54 +0200

    update black to 21.4b0

--- src/modules/localisation/localisation/vlx/sensors_sim.py
+++ src/modules/localisation/localisation/vlx/sensors_sim.py
@@ -10,10 +10,10 @@ from webots_ros2_core.webots_node import WebotsNode
 
 
 class Sensors:
-    """ Sensors class """
+    """Sensors class"""
 
     def __init__(self, node, addrs=[]):
-        """ Init Sensors """
+        """Init Sensors"""
         supervisor = node.robot + "_vlx_manager"
         environ["WEBOTS_ROBOT_NAME"] = supervisor
         self.node = VlxSupervisor(supervisor, args=None)
@@ -28,7 +28,7 @@ class Sensors:
                 print(f"No simulated VL53L1X sensor at address {addr} !")
 
     def get_distances(self):
-        """ Fetch distances from VL53L1X """
+        """Fetch distances from VL53L1X"""
         distances = {}
         for vlx, addr in zip(self._vlx_array, self._addrs):
             self.node.robot.step(1)
@@ -36,20 +36,20 @@ class Sensors:
         return distances
 
     def get_time_stamp(self):
-        """ Get time stamp from Webots """
+        """Get time stamp from Webots"""
         self.node.robot.step(0)
         t = self.node.robot.getTime()
         return Time(sec=int(t), nanosec=int((t % int(t)) * 1e9))
 
     def __del__(self):
-        """ Destructor """
+        """Destructor"""
         for vlx in self._vlx_array:
             vlx.disable()
 
 
 class VlxSupervisor(WebotsNode):
-    """ Vlx manager node """
+    """Vlx manager node"""
 
     def __init__(self, supervisor, args=None):
-        """ Init VlxSupervisor """
+        """Init VlxSupervisor"""
         super().__init__(supervisor, args)
