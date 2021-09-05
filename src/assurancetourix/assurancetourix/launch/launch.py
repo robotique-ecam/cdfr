b72327d3b6013717d2d1148daa815f16002bc159
PhileasL <phileas.lambert@gmail.com>
2021-09-05 19:43:51 +0200

    Pharaon node not launched in simulation mode (only with titan)

--- src/assurancetourix/assurancetourix/launch/launch.py
+++ src/assurancetourix/assurancetourix/launch/launch.py
@@ -7,8 +7,10 @@
 import os
 import launch
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+from platform import machine
 
 
 def generate_launch_description():
@@ -16,42 +18,45 @@ def generate_launch_description():
     params = os.path.join(
         get_package_share_directory("assurancetourix"), "param", "assurancetourix.yml"
     )
+    launch_file_dir = os.path.dirname(__file__)
+    simulation = True if machine() != "aarch64" else False
 
-    return launch.LaunchDescription(
-        [
-            DeclareLaunchArgument(
-                "params",
-                default_value=params,
-                description="Full path to param file to load",
+    launch_description = [
+        DeclareLaunchArgument(
+            "params",
+            default_value=params,
+            description="Full path to param file to load",
+        ),
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value="false",
+            description="Use simulation (Gazebo) clock if true",
+        ),
+        Node(
+            package="panoramix",
+            executable="panoramix",
+            output="screen",
+            arguments=[],
+        ),
+        Node(
+            package="strategix",
+            executable="strategix",
+            output="screen",
+            arguments=[],
+        ),
+        Node(
+            package="assurancetourix",
+            executable="assurancetourix",
+            output="screen",
+            parameters=[params],
+        ),
+    ]
+
+    if not simulation:
+        launch_description.append(
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([launch_file_dir, "/launch_external.py"]),
             ),
-            DeclareLaunchArgument(
-                "use_sim_time",
-                default_value="false",
-                description="Use simulation (Gazebo) clock if true",
-            ),
-            Node(
-                package="panoramix",
-                executable="panoramix",
-                output="screen",
-                arguments=[],
-            ),
-            Node(
-                package="strategix",
-                executable="strategix",
-                output="screen",
-                arguments=[],
-            ),
-            Node(
-                package="assurancetourix",
-                executable="assurancetourix",
-                output="screen",
-                parameters=[params],
-            ),
-            Node(
-                package="pharaon",
-                executable="pharaon",
-                output="screen",
-                parameters=[params],
-            ),
-        ]
-    )
+        )
+
+    return launch.LaunchDescription(launch_description)
