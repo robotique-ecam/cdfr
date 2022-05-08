150cb428dde4849a7c42edad7b2e406a15f7849d
PhileasL <phileas.lambert@gmail.com>
2022-05-08 17:24:04 +0000

    lh_enemies_tracker node is now lauched with assurancetourix

--- src/assurancetourix/assurancetourix/launch/launch_external.py
+++ src/assurancetourix/assurancetourix/launch/launch_external.py
@@ -6,11 +6,23 @@
 import os
 import launch
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+    params = os.path.join(
+        get_package_share_directory("lh_enemies_tracker"),
+        "param",
+        "lh_enemies_tracker.yml",
+    )
     return launch.LaunchDescription(
         [
             Node(package="pharaon", executable="pharaon", output="screen"),
-        ]
+            Node(
+                package="lh_enemies_tracker",
+                executable="lh_enemies_tracker",
+                output="screen",
+                parameters=[params],
+            ),
+        ],
     )
